20fa6355c0bba7640c35cf64cb8d98d92b42d471
wluisi <w.luisi@williamluisi.org>
2017-04-22 12:28:58 -0400

    moved content_components to proper location + added link code to cc image

--- modules/cm_bootstrap_modules/modules/content_components/modules/cc_image/templates/paragraphs-item--image.tpl.php
+++
@@ -1,9 +0,0 @@
-<section class="content-component cc--image <?php print $image['fw_class']; ?>">
-  <div class="row">
-    <div class="col-md-12">
-      <?php if ($image['img_src']): ?>
-        <img class="img-responsive" src="<?php print $image['img_src']; ?>"/>
-      <?php endif; ?>
-    </div>
-  </div>
-</section>

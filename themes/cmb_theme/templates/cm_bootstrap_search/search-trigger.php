05c17a6fa7a277a64594acf5ccfa45ccab291c2d
Jeremy Lichtman <jeremy@lichtman.ca>
2018-07-02 11:42:43 -0400

    ZD-200: Add search text to search opener. (#17)
    * ZD-200: Add search text to search opener.
    
    * Update cm-bootstrap.css
    
    * Update _search.scss

--- themes/cmb_theme/templates/cm_bootstrap_search/search-trigger.php
+++ themes/cmb_theme/templates/cm_bootstrap_search/search-trigger.php
@@ -1,5 +1,8 @@
 <div class="search-overlay-icon-wrapper">
-  <a href="#" class="search-overlay-open">
-    <i class="icon glyphicon glyphicon-search" aria-hidden="true"></i>
-  </a>
+  <div class="search-text">
+    <a href="#" class="search-overlay-open">
+      <i class="icon glyphicon glyphicon-search" aria-hidden="true"></i>
+      Search
+    </a>
+  </div>
 </div>

ff4e63f0e2251340244325a1c61df5afcce17001
ftm-bill <bill@firstturnmedia.com>
2018-07-02 12:39:11 -0400

    zd-200: fix for add search text to search trigger

--- themes/cmb_theme/templates/cm_bootstrap_search/search-trigger.php
+++ themes/cmb_theme/templates/cm_bootstrap_search/search-trigger.php
@@ -1,8 +1,6 @@
 <div class="search-overlay-icon-wrapper">
-  <div class="search-text">
-    <a href="#" class="search-overlay-open">
-      <i class="icon glyphicon glyphicon-search" aria-hidden="true"></i>
-      Search
-    </a>
-  </div>
+  <a href="#" class="search-overlay-open">
+    <i class="icon glyphicon glyphicon-search" aria-hidden="true"></i>
+    <span class="search-text">Search</span>
+  </a>
 </div>

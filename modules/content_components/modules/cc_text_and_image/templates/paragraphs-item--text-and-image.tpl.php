a2c225fb81f581cfb9b680abd7a6608226a5147f
Jeremy Lichtman <jeremy@cheekymonkeymedia.ca>
2018-09-28 12:41:27 -0400

    ZD-313: Adds various enhancements to the text and image paragraph type.

--- modules/content_components/modules/cc_text_and_image/templates/paragraphs-item--text-and-image.tpl.php
+++ modules/content_components/modules/cc_text_and_image/templates/paragraphs-item--text-and-image.tpl.php
@@ -2,38 +2,66 @@
   @media only screen {
     .img-align-right,
     .img-align-left {
-      float:none;
-      width:100%;
-      height:auto;
+      float: none;
+      width: 100%;
+      height: auto;
       margin: 0 0 20px 0;
     }
   }
   @media only screen and (min-width : 768px) {
     .img-align-right {
-      float:right;
-      width:450px;
-      height:auto;
-      margin: 0 0 20px 20px;
+      width: 100%;
+      height: auto;
+      margin: 0 0 20px 0;
+      padding: 0 0 0 20px;
     }
     .img-align-left {
-      float:left;
-      width:450px;
-      height:auto;
-      margin: 0 20px 20px 0;
+      width: 100%;
+      height: auto;
+      margin: 0;
+      padding: 0 20px 0 0;
     }
   }
 </style>
 <section class="paragraphs--text-and-image">
   <div class="row">
     <div class="col-md-12">
-      <?php if ($text_and_image['link']): ?>
-        <a target="<?php print $text_and_image['link']['target']; ?>" href="<?php print $text_and_image['link']['href']; ?>">
-          <img class="<?php print $text_and_image['image_alignment_class']; ?>" src="<?php print $text_and_image['img_src']; ?>"/>
-        </a>
+      <?php if ($text_and_image['alignment'] == 'left'): ?>
+
+        <div class="row">
+          <div class="col-md-6">
+            <?php if ($text_and_image['link']): ?>
+              <a target="<?php print $text_and_image['link']['target']; ?>" href="<?php print $text_and_image['link']['href']; ?>">
+                <img class="<?php print $text_and_image['image_alignment_class']; ?>" src="<?php print $text_and_image['img_src']; ?>"/>
+              </a>
+            <?php else: ?>
+              <img class="<?php print $text_and_image['image_alignment_class']; ?>" src="<?php print $text_and_image['img_src']; ?>"/>
+            <?php endif; ?>
+          </div>
+          <div class="col-md-6">
+            <?php print $text_and_image['field_cc_tai_text']; ?>
+          </div>
+        </div>
+
       <?php else: ?>
-        <img class="<?php print $text_and_image['image_alignment_class']; ?>" src="<?php print $text_and_image['img_src']; ?>"/>
+
+        <div class="row">
+          <div class="col-md-6">
+            <?php print $text_and_image['field_cc_tai_text']; ?>
+          </div>
+          <div class="col-md-6">
+            <?php if ($text_and_image['link']): ?>
+              <a target="<?php print $text_and_image['link']['target']; ?>" href="<?php print $text_and_image['link']['href']; ?>">
+                <img class="<?php print $text_and_image['image_alignment_class']; ?>" src="<?php print $text_and_image['img_src']; ?>"/>
+              </a>
+            <?php else: ?>
+              <img class="<?php print $text_and_image['image_alignment_class']; ?>" src="<?php print $text_and_image['img_src']; ?>"/>
+            <?php endif; ?>
+          </div>
+        </div>
+
       <?php endif; ?>
-      <?php print $text_and_image['field_cc_tai_text']; ?>
+
     </div>
   </div>
 </section>
